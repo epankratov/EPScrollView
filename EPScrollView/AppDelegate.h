8092d3fe21732f22ec684fb33ffdedaf076e657a
Eugene Pankratov <epankratov@gmail.com>
2015-03-30 22:18:08 +0300

    Initial Commit

---
+++ EPScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EPScrollView
+//
+//  Created by Eugene Pankratov on 30.03.15.
+//  Copyright (c) 2015 ua.net.pankratov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EPScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EPScrollView
+//
+//  Created by Eugene Pankratov on 30.03.15.
+//  Copyright (c) 2015 ua.net.pankratov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
