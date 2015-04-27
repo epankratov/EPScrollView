094be67262afc07b123a2edc6ce8c28d27f10a37
Eugene Pankratov <epankratov@gmail.com>
2015-04-27 16:59:41 +0300

    Add URL for images

--- EPScrollView/DataItem.h
+++ EPScrollView/DataItem.h
@@ -15,6 +15,7 @@
 @property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) NSString *additionalTitle;
 @property (nonatomic, strong) NSString *synopsis;
+@property (nonatomic, strong) NSString *imageURL;
 
 - (instancetype)initWithNumber:(NSInteger)number;
 
